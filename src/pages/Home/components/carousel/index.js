df90d1512be008d5845e677faafc838646b72e9b
Kevin Chan <kevinchan950120@gmail.com>
2022-04-01 00:43:14 +0800

    [Home] Updated

--- src/pages/Home/components/carousel/index.js
+++ src/pages/Home/components/carousel/index.js
@@ -7,7 +7,6 @@ export const Carousel = () => (
       <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
       <div class="carousel-inner">
         <div class="carousel-item active jimmy-background" data-bs-interval="4000">
@@ -21,18 +20,10 @@ export const Carousel = () => (
             </p>
           </div>
         </div>
-        <div class="carousel-item" data-bs-interval="4000">
-          <img src="https://firebasestorage.googleapis.com/v0/b/myprofile-3d66c.appspot.com/o/frank-mckenna-OD9EOzfSOh0-unsplash.jpg?alt=media&token=1297b479-ef89-45c6-8938-0367d3f7c99c" class="d-block w-100 image-center" alt=''/>
-          <div class="carousel-caption d-none d-md-block freedom-font">
-            <h2>Freedom</h2>
-            <p>Credit: <a href='https://unsplash.com/@frankiefoto' style={{ color: 'black'}}>frank mckenna</a></p>
-          </div>
-        </div>
-        <div class="carousel-item" data-bs-interval="4000">
-          <img src="https://firebasestorage.googleapis.com/v0/b/myprofile-3d66c.appspot.com/o/leon-contreras-Wu3pkvOIMJk-unsplash.jpg?alt=media&token=44788074-8f2d-4613-8da9-c912a9d4d50e" class="d-block w-100" alt=''/>
-          <div class="carousel-caption d-none d-md-block hope-font">
-            <h2>Light of Hope</h2>
-            <p>Credit: <a href='https://unsplash.com/@lc_photography' style={{ color: 'white'}}>Leon Contreras</a></p>
+        <div class="carousel-item mechanical-hand-background" data-bs-interval="4000">
+          <img src="https://firebasestorage.googleapis.com/v0/b/myprofile-3d66c.appspot.com/o/mecha%20hand%20illustration%20final%20Production.bmp?alt=media&token=8d8e5033-06a8-43f7-ac22-3b70cdeed320" class="d-block h-100 image-center" alt=''/>
+          <div class="carousel-caption d-none d-md-block mechanical-hand-font">
+            <p>@<a href='https://instagram.com/qbdpjim?utm_medium=copy_link' style={{ color: 'black'}} target="_blank">qbdpjim</a></p>
           </div>
         </div>
       </div>
